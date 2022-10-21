ab4ae927ce1d71fc9b7babae887ce7aa633610fd
Savon Williams <44207109+Savon817@users.noreply.github.com>
2022-10-20 20:24:31 -0500

    Updated Header, and Made landingPage component

--- music-app/src/app/app-routing.module.ts
+++ music-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MusicListComponent } from './music-list/music-list.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: LandingPageComponent},
+  {path: 'musiclist', component: MusicListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- music-app/src/app/app.module.ts
+++ music-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MusicListComponent } from './music-list/music-list.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MusicListComponent
+    MusicListComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
