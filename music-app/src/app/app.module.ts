2b40fe38f8a1985edfcda12380736c698f55a941
Savon Williams <44207109+Savon817@users.noreply.github.com>
2022-10-20 19:59:31 -0500

    Update Navbar, Added Music list component

--- music-app/src/app/app.module.ts
+++ music-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MusicListComponent } from './music-list/music-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MusicListComponent
   ],
   imports: [
     BrowserModule,
