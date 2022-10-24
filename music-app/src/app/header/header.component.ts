9968cac9f67a9694b8f32fbadf3f673c41a9f798
Savon817 <44207109+Savon817@users.noreply.github.com>
2022-10-24 13:55:04 -0500

    Update header.component.ts

--- music-app/src/app/header/header.component.ts
+++ music-app/src/app/header/header.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  svgPath1: string = "M2.5 5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1Zm2 0a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1Zm7.5-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Zm1.5.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1Zm-7-1a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3Zm5.5 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Z";
 
   constructor() { }
 
